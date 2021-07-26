1763b616e9f9917810324c0a67d6a9dab6ff2224
Arsh <arshradhanpura28@gmail.com>
2021-07-26 23:36:54 +0530

    commit

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+// i have taken data in JSON format so in Postman go to headers and set Content-Type : application/json
+
+//FOR /add route POST as "task" : "task 1"
+
+// FOR /remove route POST as "location" : "2" (deletes the task at specified index)
+
+// FOR /update route POST as (updates with the task provided at specified index)
+// {
+//     "task" : "task new",
+//     "location" : "3"
+// }
+
+//SORRY I added node modules by mistake in the staging area
+
 const express = require('express');
 const app = express();
 app.use(express.json({ extended: true }));
@@ -20,19 +34,27 @@ app.post('/add', function (req, res) {
 
 //remove task by location
 app.post('/remove', function (req, res) {
-  const location = req.body.location;
-  list.splice(location, 1);
-  console.log('list is ' + list);
-  res.json(list);
+  if (list.length != 0) {
+    const location = req.body.location;
+    list.splice(location, 1);
+    console.log('list is ' + list);
+    res.json(list);
+  } else {
+    res.send('ERROR');
+  }
 });
 
 //update task by location
 app.post('/update', function (req, res) {
   const task = req.body.task;
   const location = req.body.location;
-  list[location] = req.body.task;
-  console.log('list is ' + list);
-  res.json(list);
+  if (list.length != 0 && location <= list.length) {
+    list[location] = req.body.task;
+    console.log('list is ' + list);
+    res.json(list);
+  } else {
+    res.send('ERROR');
+  }
 });
 
 app.listen(3000, function () {
